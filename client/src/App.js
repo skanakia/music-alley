a1a4ed9fac35cd0f77a5ac9ee87bceae69159b88
skanakia <skanakia1@gmail.com>
2018-07-31 10:36:31 -0400

    fixed result rendering

--- client/src/App.js
+++ client/src/App.js
@@ -31,15 +31,15 @@ class App extends Component {
   }
 
   getUser() {
-    axios.get('/user/').then(response => {
+    axios.get('http://3001/api/audio/user').then(response => {
       console.log('Get user response: ')
       console.log(response.data)
-      if (response.data.user) {
+      if (response.data) {
         console.log('Get User: There is a user saved in the server session: ')
 
         this.setState({
           loggedIn: true,
-          username: response.data.user.username
+          username: response.data.username
         })
       } else {
         console.log('Get user: no user');
--- client/src/components/results/results.js
+++ client/src/components/results/results.js
@@ -6,46 +6,48 @@ import "./results.css"
 
 class Results extends Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        id: props.id,
-        recordings: []
-      }
+        super(props);
+        this.state = {
+            id: props.id,
+            recordings: []
+        }
     }
-   
-  componentDidMount() {
 
-    API.getFilesByProject(this.state.id).then(response => {
-        console.log(response)
-        
-        this.setState({
-            recordings: response.data
+    componentDidMount() {
+
+        API.getFilesByProject(this.state.id).then(response => {
+            console.log(response)
+
+            this.setState({
+                recordings: response.data
+            })
+            console.log("SHAPE:", this.state.recordings)
         })
-        console.log("SHAPE:", this.state.recordings)
-    })
-
-    
-  }
 
 
-   
+    }
+
+
+
     render() {
 
 
-      return (
-        <Wrapper>
-        <div className="audio-result">
-          {this.state.recordings.map(element => {
-            //   <ResultCard
-            //     id={this.state.id}
-            //     audio={element.file_url}
-            //   />
-            <div>Hello</div>
-          })}
-        </div>
-        </Wrapper>
-      );
+        return (
+
+            <div className="audio-result">
+                <Wrapper>
+                    {this.state.recordings.forEach(element => (
+                        <ResultCard
+                            id={this.state.id}
+                            audio={element.file_url}
+                        />
+                        // <div>Hello</div>
+                    ))}
+                </Wrapper>
+            </div>
+
+        );
     }
-  }
+}
 
 export default Results;
