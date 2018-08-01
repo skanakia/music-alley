bf626ef470c590feb2db7dbb2cf14d1344741f06
skanakia <skanakia1@gmail.com>
2018-08-01 16:20:44 -0400

    minor changes

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.use(routes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + 'client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
